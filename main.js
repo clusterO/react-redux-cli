ab507e0e4908c000b4fdae6bf65b7cdcb8d7dd47
cluserO <fourmou.m@gmail.com>
2020-01-30 20:05:54 +0100

    Fix cmds names & refactore

--- main.js
+++ main.js
@@ -4,76 +4,79 @@ const fs = require("fs").promises;
 const REACT = require("./react");
 const REDUX = require("./redux");
 
+const reactCommands = ["fc", "fcr", "cc", "ccr"];
+const reduxCommands = ["rs", "rt", "rd"];
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
 rl.question("rr ", userInput => {
-  let commands = userInput.split(" ");
+  reactCommands.forEach(cmd => {
+    excuteReactCommand(cmd, userInput);
+  });
 
-  if (commands.indexOf("cf") !== -1) {
-    let componentName = commands[commands.indexOf("cf") + 1];
-
-    if (commands.indexOf("+") === -1)
-      fs.writeFile(
-        path.join(__dirname, `${componentName}.js`),
-        REACT.FC(componentName)
-      );
-    else fs.appendFile(path.join(__filename), REACT.CF(componentName));
-  }
-
-  if (commands.indexOf("cfr") !== -1) {
-    let componentName = commands[commands.indexOf("cfr") + 1];
-
-    if (commands.indexOf("+") === -1)
-      fs.writeFile(
-        path.join(__dirname, `${componentName}.js`),
-        REACT.FCR(componentName)
-      );
-    else fs.appendFile(path.join(__filename), REACT.CFR(componentName));
-  }
-
-  if (commands.indexOf("cc") !== -1) {
-    let componentName = commands[commands.indexOf("cc") + 1];
-
-    if (commands.indexOf("+") === -1)
-      fs.writeFile(
-        path.join(__dirname, `${componentName}.js`),
-        REACT.CC(componentName)
-      );
-    else fs.appendFile(path.join(__filename), REACT.CC(componentName));
-  }
-
-  if (commands.indexOf("ccr") !== -1) {
-    let componentName = commands[commands.indexOf("ccr") + 1];
-
-    if (commands.indexOf("+") === -1)
-      fs.writeFile(
-        path.join(__dirname, `${componentName}.js`),
-        REACT.CCR(componentName)
-      );
-    else fs.appendFile(path.join(__filename), REACT.CCR(componentName));
-  }
-
-  if (commands.indexOf("rs") !== -1) {
-    fs.writeFile(path.join(__dirname, "store.js"), REDUX.RS());
-    fs.writeFile(path.join(__dirname, "types.js"), "");
-  }
-
-  if (commands.indexOf("rt") !== -1) {
-    let type = commands[commands.indexOf("rt") + 1];
-
-    fs.appendFile(path.join(__dirname, "types.js"), REDUX.RT(type));
-  }
-
-  if (commands.indexOf("rd") !== -1) {
-    REDUX.RR(path.join(__dirname, "types.js"))
-      .then(data => {
-        fs.writeFile(path.join(__dirname, "reducers.js"), data);
-      })
-      .catch(err => console.error(err));
-  }
+  reduxCommands.forEach(cmd => {
+    excuteReduxCommand(cmd, userInput);
+  });
 
   rl.close();
 });
+
+let excuteReactCommand = (cmd, userInput) => {
+  let commands = userInput.split(" ");
+
+  if (commands.indexOf(cmd) !== -1) {
+    let componentName = commands[commands.indexOf(cmd) + 1];
+    let execute;
+
+    switch (cmd) {
+      case "fc":
+        execute = REACT.FC;
+        break;
+      case "fcr":
+        execute = REACT.FCR;
+        break;
+      case "cc":
+        execute = REACT.CC;
+        break;
+      case "ccr":
+        execute = REACT.CCR;
+        break;
+    }
+
+    if (commands.indexOf("+") === -1)
+      fs.writeFile(
+        path.join(__dirname, `${componentName}.js`),
+        execute(componentName)
+      );
+    else fs.appendFile(path.join(__filename), execute(componentName));
+  }
+};
+
+let excuteReduxCommand = (cmd, userInput) => {
+  let commands = userInput.split(" ");
+
+  if (commands.indexOf(cmd) !== -1) {
+    let execute;
+
+    switch (cmd) {
+      case "rs":
+        fs.writeFile(path.join(__dirname, "store.js"), REDUX.RS());
+        fs.writeFile(path.join(__dirname, "types.js"), "");
+        break;
+      case "rt":
+        let type = commands[commands.indexOf("rt") + 1];
+        fs.appendFile(path.join(__dirname, "types.js"), REDUX.RT(type));
+        break;
+      case "rd":
+        REDUX.RR(path.join(__dirname, "types.js"))
+          .then(data => {
+            fs.writeFile(path.join(__dirname, "reducers.js"), data);
+          })
+          .catch(err => console.error(err));
+        break;
+    }
+  }
+};
